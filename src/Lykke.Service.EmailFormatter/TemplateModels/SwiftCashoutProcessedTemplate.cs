fbf6b5dd60e8c11e5300b4b37534865b5514aafa
Yury Batsyuro <yury.batsyuro@lykke.com>
2017-07-31 23:40:48 +0300

    Adjusted templates and added SWIFT-cashout

---
+++ src/Lykke.Service.EmailFormatter/TemplateModels/SwiftCashoutProcessedTemplate.cs
@@ -0,0 +1,15 @@
+﻿namespace Lykke.Service.EmailFormatter.TemplateModels
+{
+    public class SwiftCashOutProcessedTemplate
+    {
+        public string AssetId { get; set; }
+        public double Amount { get; set; }
+        public string FullName { get; set; }
+        public string Bic { get; set; }
+        public string AccNum { get; set; }
+        public string AccName { get; set; }
+        public string BankName { get; set; }
+        public string AccHolderAddress { get; set; }
+        public int Year { get; internal set; }
+    }
+}
