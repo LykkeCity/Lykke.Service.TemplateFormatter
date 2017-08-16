5f054446667c476f8051ef63cff7c3a2a0acfb71
Oleksandr Lysenko <oleksandr.lysenko@pl.pwc.com>
2017-08-16 16:59:01 +0200

    change templates: RequestForDocumentTemplate, SwiftCashoutProcessedTemplate

--- src/Lykke.Service.EmailFormatter/TemplateModels/SwiftCashoutProcessedTemplate.cs
+++ src/Lykke.Service.EmailFormatter/TemplateModels/SwiftCashoutProcessedTemplate.cs
@@ -2,14 +2,7 @@
 {
     public class SwiftCashoutProcessedTemplate
     {
-        public string AssetId { get; set; }
-        public double Amount { get; set; }
         public string FullName { get; set; }
-        public string Bic { get; set; }
-        public string AccNum { get; set; }
-        public string AccName { get; set; }
-        public string BankName { get; set; }
-        public string AccHolderAddress { get; set; }
         public int Year { get; set; }
     }
 }
