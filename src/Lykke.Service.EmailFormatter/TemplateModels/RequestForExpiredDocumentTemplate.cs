060bfecc1ef967b50bbd1f92ca034140827c10e5
igorag <igorag@mmtr.ru>
2017-10-13 17:04:51 +0300

    LWDEV-3027-3038 Docs renewal

---
+++ src/Lykke.Service.EmailFormatter/TemplateModels/RequestForExpiredDocumentTemplate.cs
@@ -0,0 +1,9 @@
+﻿namespace Lykke.Service.EmailFormatter.TemplateModels
+{
+    public class RequestForExpiredDocumentTemplate
+    {
+        public string FullName { get; set; }
+        public string Text { get; set; }
+        public int Year { get; set; }
+    }
+}
