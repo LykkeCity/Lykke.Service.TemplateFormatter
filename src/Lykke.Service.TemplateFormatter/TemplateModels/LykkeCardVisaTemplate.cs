6acf62f0b63189eb39649ad7a0c7e3d259b9f3ca
fry <fry@FRY-PC>
2017-11-24 16:45:37 +0200

    Added LykkeCardVisaTemplate

---
+++ src/Lykke.Service.TemplateFormatter/TemplateModels/LykkeCardVisaTemplate.cs
@@ -0,0 +1,8 @@
+﻿namespace Lykke.Service.TemplateFormatter.TemplateModels
+{
+    public class LykkeCardVisaTemplate
+    {
+        public string Url { get; set; }
+        public int Year { get; set; }
+    }
+}
