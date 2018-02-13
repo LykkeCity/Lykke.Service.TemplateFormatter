ae49e3c0a797a533176152a63899b5dae38d80db
igorag <igorag@mmtr.ru>
2018-02-13 16:54:17 +0300

    Invalid located template deleted

--- src/Lykke.Service.TemplateFormatter.Client/TemplateModels/RestrictedAreaTemplate.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lykke.Service.TemplateFormatter.TemplateModels
-{
-    public class RestrictedAreaTemplate
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public int Year { get; set; }
-    }
-}
