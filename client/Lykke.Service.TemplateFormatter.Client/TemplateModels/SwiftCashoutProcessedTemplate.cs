93c6e7ef9ee08a4923953e465f7c7720c300ff0f
igorag <igorag@mmtr.ru>
2018-04-16 18:16:56 +0300

    LWDEV-6921-Withdrawal-Email

--- client/Lykke.Service.TemplateFormatter.Client/TemplateModels/SwiftCashoutProcessedTemplate.cs
+++ client/Lykke.Service.TemplateFormatter.Client/TemplateModels/SwiftCashoutProcessedTemplate.cs
@@ -3,6 +3,7 @@
     public class SwiftCashoutProcessedTemplate
     {
         public string FullName { get; set; }
+        public string DateOfWithdrawal { get; set; }
         public string Year { get; set; }
     }
 }
